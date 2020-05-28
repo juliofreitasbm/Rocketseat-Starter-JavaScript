db874bc52bed19f459106d35937bd922f125ca6f
Julio <juliofreitas.bm@gmail.com>
2020-05-28 08:00:54 -0300

    App de Todos - Criando Todos/Excluindo Todos

--- 3_app/todos.js
+++ 3_app/todos.js
@@ -9,13 +9,44 @@ let todos = [
 ]
 
 function renderTodos() {
+	//Limpando a lista antes de renderizar
+	listElement.innerHTML = '';
+
 	for(todo of todos){
+		//Criando Item da Lista e criando Texto que será adicionado ao item (pegando do vetor)
 		let todoElement = document.createElement('li');
 		let todoText = document.createTextNode(todo);
 
+		//Adicionando botão de excluir
+		let linkElement = document.createElement('a');
+		let linkText = document.createTextNode('Excluir');
+		linkElement.setAttribute('href', '#')
+		linkElement.appendChild(linkText)
+
+		//Fazendo o botão de exluir funcionar
+		var pos = todos.indexOf(todo);
+		linkElement.setAttribute('onclick','deleteTodo(' + pos + ')' )
+
+		//Adicionando texto ao Item, adicionando botão de excluir ao Item, adicionando Item à Lista
 		todoElement.appendChild(todoText);
+		todoElement.appendChild(linkElement);
 		listElement.appendChild(todoElement);
 	}
 }
 
 renderTodos();
+
+function addTodo() {
+	let todoText = inputElement.value;
+
+	todos.push(todoText);
+	inputElement.value = '';
+	renderTodos();
+}
+buttonElement.onclick = addTodo;
+
+function deleteTodo(pos) {
+	//metodo que remove itens de um array. Parâmetros (A partir dessa posição, Remova essa quantidade de itens)
+	todos.splice(pos, 1);
+	renderTodos();
+}
