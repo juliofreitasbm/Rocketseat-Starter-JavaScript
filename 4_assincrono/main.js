e44b76605c9c3a0f0f91cad6467118477a53e4b7
Julio <juliofreitas.bm@gmail.com>
2020-05-28 12:04:07 -0300

    JS assíncrono - Utilizando Axios

--- 4_assincrono/main.js
+++ 4_assincrono/main.js
@@ -1,28 +1,6 @@
-//Promisses são funções que não vão influenciar na linha do tempo do código Javascript
-//São funções que vão devolver o resultado seja de sucesso ou erro só depois de um tempo e a gente não precisa se preocupar quando esse valor será retornado pois o JavaScript continuará executando normalmente
-var minhaPromise = function() {
-	//Promisse é uma Class
-	//resolve é a função que vai ser usada dentro da promisse quando o resultado da requisição foi de sucesso
-	//reject é a função que vai ser usada dentro da promisse quando o resultado da requisição não foi de sucesso
-	return new Promise(function(resolve, reject) {
-		var xhr = new XMLHttpRequest();
-		xhr.open('GET', 'https://api.github.com/users/juliofreitasbm');
-		xhr.send(null);
-	
-		xhr.onreadystatechange = function() {
-			if(xhr.readyState === 4) {
-				if(xhr.status === 200) {
-					resolve(JSON.parse(xhr.responseText));
-				} else
-					reject('Erro na requisição');
-			}
-		}
-	});
-}
-
-minhaPromise()
+axios.get('https://api.github.com/users/juliofreitasbm')
 	.then(function(response) {
-		console.log(response);
+		console.log(response.data.avatar_url);
 	})
 	.catch(function(error) {
 		console.warn(error)
