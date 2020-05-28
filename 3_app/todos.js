7d0e0ede43fc02bd261fe459babfe29c8908d32d
Julio <juliofreitas.bm@gmail.com>
2020-05-28 07:21:19 -0300

    App de Todos - Renderizando Todos

--- 3_app/todos.js
+++ 3_app/todos.js
@@ -2,8 +2,20 @@ let listElement = document.querySelector('#app ul');
 let inputElement = document.querySelector('#app input');
 let buttonElement = document.querySelector('#app button');
 
-var todos = [
+let todos = [
 	'Fazer café',
 	'Estudar JavaScript',
 	'Acessar comunidade da Rocketseat'
 ]
+
+function renderTodos() {
+	for(todo of todos){
+		let todoElement = document.createElement('li');
+		let todoText = document.createTextNode(todo);
+
+		todoElement.appendChild(todoText);
+		listElement.appendChild(todoElement);
+	}
+}
+
+renderTodos();
