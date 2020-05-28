4cf09e7dc765b52d04ae043c901f9167d5926eb4
Julio <juliofreitas.bm@gmail.com>
2020-05-28 07:07:26 -0300

    App de Todos - Iniciando aplicação

--- 3_app/todos.js
+++ 3_app/todos.js
@@ -1,1 +1,9 @@
-alert('funcionou!');
+let listElement = document.querySelector('#app ul');
+let inputElement = document.querySelector('#app input');
+let buttonElement = document.querySelector('#app button');
+
+var todos = [
+	'Fazer café',
+	'Estudar JavaScript',
+	'Acessar comunidade da Rocketseat'
+]
