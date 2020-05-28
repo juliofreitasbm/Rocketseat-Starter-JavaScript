d06bfd0eb795330e6d85c034cd683f0a13d80092
Julio <juliofreitas.bm@gmail.com>
2020-05-28 11:40:12 -0300

    JS assíncrono - Promises

--- 4_assincrono/main.js
+++ 4_assincrono/main.js
@@ -1,15 +1,29 @@
-var xhr = new XMLHttpRequest();
-
-//Utilizando método GET para consumir dados da API do GitHub
-xhr.open('GET', 'https://api.github.com/users/juliofreitasbm')
-xhr.send(null);
-
-//Configurando o script pra esperar os dados da API
-xhr.onreadystatechange = function() {
-	//4 é a variável que significa que a resposta voltou
-	if(xhr.readyState === 4) {
-		//converter o JSON que retorna do servidor para um objeto ou vetor
-		console.log(JSON.parse(xhr.responseText))
-	}
-
+//Promisses são funções que não vão influenciar na linha do tempo do código Javascript
+//São funções que vão devolver o resultado seja de sucesso ou erro só depois de um tempo e a gente não precisa se preocupar quando esse valor será retornado pois o JavaScript continuará executando normalmente
+var minhaPromise = function() {
+	//Promisse é uma Class
+	//resolve é a função que vai ser usada dentro da promisse quando o resultado da requisição foi de sucesso
+	//reject é a função que vai ser usada dentro da promisse quando o resultado da requisição não foi de sucesso
+	return new Promise(function(resolve, reject) {
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', 'https://api.github.com/users/juliofreitasbm');
+		xhr.send(null);
+	
+		xhr.onreadystatechange = function() {
+			if(xhr.readyState === 4) {
+				if(xhr.status === 200) {
+					resolve(JSON.parse(xhr.responseText));
+				} else
+					reject('Erro na requisição');
+			}
+		}
+	});
 }
+
+minhaPromise()
+	.then(function(response) {
+		console.log(response);
+	})
+	.catch(function(error) {
+		console.warn(error)
+	});
